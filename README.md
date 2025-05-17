f5efc3e2d5f51fa06ce0336df29cc7211cd1d70b
Gustavo Lizarraga <gustavo.lizarraga@gmail.com>
2025-05-17 09:30:53 -0300

    Cambio
    Subo varios cambios para la entrega final

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# CoderHouse Proyecto Javascript - Segunda Entrega
+# CoderHouse Proyecto Javascript - Entrega Final
 
-Proyecto desarrollado para CoderHouse donde usamos comandos Javascript, eventos, localstorage, array, metodos de array como ser find, reduce, filter, map, iteradores. Se trata de un Carrito de Compras en el cual te permite elegir diferenetes productos y su cantidad y al final te suma y te dice el total por cada uno que elegiste.
+Proyecto desarrollado para CoderHouse donde usamos comandos Javascript, eventos, localstorage, array, metodos de array como ser find, reduce, filter, map, iteradores.
+Se uso Promise para llamar a una api y cargar los productos e imagenes.
+Se utilizo la api https://fakestoreapi.com/products para cargar las imagenes.
+Se uso swetalert2 para mejorar la experiencia de usuario y interfaz.
+Se utilizo try-catch en el llamado de la api por si da error. Emite un alert de tipo swetalert2 indicando el error.
+Se emite un error utilizando "throw new Error"
+Se utilizo Toastify para los mensajes flotantes.
+Se utilizo fontawesome para lo iconos en los botones.
+Se utilizo luxon para poner la fecha y hora del dia. Se puso un reloj en el header.
+Se utilizo setInterval para mantener actualizado el reloj.
+
+Se trata de un Carrito de Compras en el cual te permite elegir diferenetes productos y su cantidad y al final te suma y te dice el total por cada uno que elegiste.
 
 Utilice bootstrap de forma local.
 
@@ -11,7 +22,7 @@ El proyecto tiene 1 páginas:
 ## Carrito de Compras
 
 En esta página se uso funciones y métodos convencionales de javascript para darle vida al Carrito de Compra.
-Se utilizo un array donde se setean todos los productos con su precio e imagenes.
+Se utilizo una api de donde se obtiene todos los productos con su precio, descripción e imagenes.
 Se pide el usuario apenas ingresa al sitio y luego se lo guarda en localstorage
 Permite agregar productos al carrito, quitar uno a uno y sumar uno a uno a la lista de los productos agregados.
 Al finalizar la compra muestra un alert que para ellos se uso la libreria sweetalert2
--- js/index.js
+++ js/index.js
@@ -1,103 +1,121 @@
-// Lista de productos disponibles
-const productos = [
-  {
-    id: 1,
-    nombre: "Reloj Smart Pro",
-    descripcion:
-      "El Reloj Smart Pro es el smartwatch ideal para quienes buscan funcionalidad, diseño y rendimiento en un solo dispositivo. Con una pantalla AMOLED de alta resolución, monitoreo de salud y conectividad avanzada, es el complemento perfecto para tu día a día. ",
-    precio: 199.0,
-    imagen: "producto1",
-    actualizacion: "Ultima actualizacion 3 min atras",
-  },
-  {
-    id: 2,
-    nombre: "Auriculares Wireless",
-    descripcion:
-      "Audio de alta calidad con bajos potentes y agudos nítidos. Bluetooth 5.0 para conexión estable y sin cortes. Batería de larga duración, ideal para todo el día. Diseño ergonómico y ligero para máxima comodidad. Resistentes al agua, perfectos para entrenar. Micrófono incorporado para llamadas claras. ",
-    precio: 149.0,
-    imagen: "producto2",
-    actualizacion: "Ultima actualizacion 5 min atras",
-  },
-  {
-    id: 3,
-    nombre: "Cámara Retro X100",
-    descripcion:
-      "Sensor de alta resolución para fotos nítidas y vibrantes. Diseño vintage con acabados premium y controles manuales. Lente de alta calidad para capturar cada detalle con precisión. Grabación en Full HD para videos con un look cinematográfico. Conectividad WiFi y Bluetooth para compartir al instante. ",
-    precio: 299.0,
-    imagen: "producto3",
-    actualizacion: "Ultima actualizacion 10 min atras",
-  },
-  {
-    id: 4,
-    nombre: "Smartphone Ultra",
-    descripcion:
-      "Pantalla AMOLED Full HD+ para una experiencia visual inmersiva. Procesador de alto rendimiento para máxima velocidad y fluidez. Cámara de ultra resolución para fotos y videos impresionantes. Batería de larga duración con carga rápida. Desbloqueo facial y sensor de huellas para mayor seguridad. Conectividad 5G para navegar sin límites.",
-    precio: 599.0,
-    imagen: "producto4",
-    actualizacion: "Ultima actualizacion 7 min atras",
-  },
-];
+// Función para obtener productos desde la API
+let productosAPI = [];
+
+function obtenerProductos() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (!response.ok) {
+        throw new Error("Error al obtener los productos");
+      }
+      const data = await response.json();
+      productosAPI = data.map((producto) => ({
+        id: producto.id,
+        nombre: producto.title,
+        descripcion: producto.description,
+        precio: producto.price,
+        imagen: producto.image,
+        actualizacion: "Actualizado recientemente",
+      }));
+      Toastify({
+        text: "Productos cargados Exitosamente",
+        duration: 2000,
+        gravity: "bottom",
+      }).showToast();
+
+      resolve(productosAPI);
+    } catch (error) {
+      reject(
+        new Error(
+          "No se pudo conectar a la API. Por favor, intente nuevamente más tarde."
+        )
+      );
+    }
+  });
+}
 
 // Array para almacenar las elecciones del usuario
 let carrito = [];
 
-// Función para renderizar los productos en el div con id "productos"
+// Modificar la función para renderizar productos desde la API
 function renderizarProductos() {
-  const contenedorProductos = document.getElementById("productos");
-  productos.forEach((producto) => {
-    const card = document.createElement("div");
-    card.className = "card mb-3";
-    card.innerHTML = `
-      <div class="card">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img
-              src="img/productos/${producto.imagen}.jpg"
-              class="card-img-top"
-              alt="${producto.nombre}"
-            />                  
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <div class="card-body">                      
-                <h5 class="card-title">${producto.nombre}</h5>                      
-                <p class="card-text">${producto.descripcion}</p>
-                <p class="card-text">
-                  Precio: <strong>$${producto.precio}</strong>
-                  <small class="text-body-secondary"
-                    >${producto.actualizacion}</small
-                  >
-                </p>
-                <button
-                  class="btn btn-primary ch-btn-primary"
-                  style="float: right; bottom: 4px; position: relative"
-                  onclick="agregarAlCarrito(${producto.id})"
-                >
-                  Agregar al carrito
-                </button>                      
+  obtenerProductos()
+    .then((productosAPI) => {
+      const contenedorProductos = document.getElementById("productos");
+      productosAPI.forEach((producto) => {
+        const card = document.createElement("div");
+        card.className = "card mb-3";
+        card.innerHTML = `
+          <div class="card">
+            <div class="row g-0">
+              <div class="col-md-4 d-flex align-items-center justify-content-center">
+                <img
+                  src="${producto.imagen}"
+                  class="card-img-top img-fluid"
+                  alt="${producto.nombre}"
+                  style="max-height: 150px; object-fit: contain;"
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p class="card-text">${producto.descripcion}</p>
+                    <p class="card-text">
+                      Precio: <strong>$${producto.precio}</strong>
+                    </p>
+                    <p class="card-text">
+                      <small class="text-body-secondary">${producto.actualizacion}</small>
+                    </p>
+                    <button
+                      class="btn btn-primary ch-btn-primary"
+                      style="float: right; bottom: 4px; position: relative"
+                      onclick="agregarAlCarrito(${producto.id})"
+                    >
+                      <i class="fa-solid fa-cart-plus"></i> Agregar al carrito
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    `;
-    contenedorProductos.appendChild(card);
-  });
+        `;
+        contenedorProductos.appendChild(card);
+      });
+    })
+    .catch((error) => {
+      Swal.fire({
+        title: "Carga de Carrito",
+        text: error,
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
+    });
 }
 
 // Función para agregar un producto al carrito y actualizar la tabla
 function agregarAlCarrito(productoId) {
-  const producto = productos.find((p) => p.id === productoId);
+  const producto = productosAPI.find((p) => p.id === productoId);
   if (producto) {
     let itemCarrito = carrito.find((item) => item.producto === producto.nombre);
     if (itemCarrito) {
       itemCarrito.cantidad += 1;
+      Toastify({
+        text: "Se sumo un articulo mas al producto: " + producto.nombre,
+        duration: 3000,
+        gravity: "bottom",
+      }).showToast();
     } else {
       carrito.push({
         producto: producto.nombre,
         cantidad: 1,
         precio: producto.precio,
       });
+      Toastify({
+        text: producto.nombre + " agregado correctamente al carrito",
+        duration: 3000,
+        gravity: "bottom",
+      }).showToast();
     }
     actualizarTablaCarrito();
   }
@@ -105,11 +123,16 @@ function agregarAlCarrito(productoId) {
 
 // Función para quitar un producto del carrito
 function quitarDelCarrito(productoId) {
-  const producto = productos.find((p) => p.id === productoId);
+  const producto = productosAPI.find((p) => p.id === productoId);
   if (producto) {
     let itemCarrito = carrito.find((item) => item.producto === producto.nombre);
     if (itemCarrito) {
       itemCarrito.cantidad -= 1;
+      Toastify({
+        text: "Se quito un articulo mas al producto: " + producto.nombre,
+        duration: 3000,
+        gravity: "bottom",
+      }).showToast();
       if (itemCarrito.cantidad <= 0) {
         carrito = carrito.filter((item) => item.producto !== producto.nombre);
       }
@@ -120,7 +143,7 @@ function quitarDelCarrito(productoId) {
 
 // Función para eliminar un producto completamente del carrito
 function eliminarDelCarrito(productoId) {
-  const producto = productos.find((p) => p.id === productoId);
+  const producto = productosAPI.find((p) => p.id === productoId);
   if (producto) {
     carrito = carrito.filter((item) => item.producto !== producto.nombre);
     actualizarTablaCarrito();
@@ -135,7 +158,7 @@ function actualizarTablaCarrito() {
   let totalPagar = 0;
 
   carrito.forEach((item) => {
-    const producto = productos.find((p) => p.nombre === item.producto);
+    const producto = productosAPI.find((p) => p.nombre === item.producto);
     const fila = document.createElement("tr");
     fila.innerHTML = `
       <td>${item.producto}</td>
@@ -155,7 +178,7 @@ function actualizarTablaCarrito() {
       <td>
         <button class="btn btn-sm btn-warning" onclick="eliminarDelCarrito(${
           producto.id
-        })">Eliminar</button>
+        })"><i class="fa-solid fa-trash"></i></button>
       </td>
     `;
     tablaCarrito.appendChild(fila);
@@ -201,7 +224,7 @@ function finalizarCompra() {
 
 // Función para pedir el nombre del cliente y guardarlo en localStorage
 function solicitarNombreCliente() {
-  const nombreGuardado = localStorage.getItem("nombreCliente");
+  const nombreGuardado = localStorage.getItem("nombreClienteFinal");
   if (!nombreGuardado) {
     Swal.fire({
       title: "Bienvenido",
@@ -213,7 +236,7 @@ function solicitarNombreCliente() {
       allowOutsideClick: false,
     }).then((result) => {
       if (result.value) {
-        localStorage.setItem("nombreCliente", result.value);
+        localStorage.setItem("nombreClienteFinal", result.value);
         mostrarNombreCliente(result.value);
       }
     });
@@ -225,10 +248,23 @@ function solicitarNombreCliente() {
 // Función para mostrar el nombre del cliente en el header
 function mostrarNombreCliente(nombre) {
   const header = document.querySelector("header .container-fluid");
+  const contenedor = document.createElement("div");
+  contenedor.className = "text-end text-white";
+
   const saludo = document.createElement("div");
-  saludo.className = "text-end text-white";
   saludo.innerHTML = `<strong>Hola, ${nombre}!</strong>`;
-  header.appendChild(saludo);
+
+  const fechaHora = document.createElement("div");
+  setInterval(() => {
+    const now = luxon.DateTime.now().toLocaleString(
+      luxon.DateTime.DATETIME_MED
+    );
+    fechaHora.innerHTML = `<small>${now}</small>`;
+  }, 1000);
+
+  contenedor.appendChild(saludo);
+  contenedor.appendChild(fechaHora);
+  header.appendChild(contenedor);
 }
 
 // Renderizar los productos al cargar la página
